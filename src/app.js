fb41d379d2e913ac3031656ef6d731a6f0442afc
EstevaoCalera <49175179+EstevaoCalera@users.noreply.github.com>
2021-04-12 10:59:33 -0300

    Delete src directory

--- src/app.js
+++
@@ -1,5 +0,0 @@
-function converter() {
-    let input = document.getElementById("input");
-    output = input.value/5.6;
-    document.getElementById("output").innerHTML = "U$ " + output.toFixed(2);
-}
